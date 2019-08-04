74f6de8ad6ff55c0c05043afcf6f62e5fe51f0d2
zeqiang.chen <1209120868@qq.com>
2019-08-04 11:47:48 +0800

    [zeqiang] feat: 完成首页笔记列表的显示

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.less';
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from "./home/Home";
+import NoteDetail from "./note/NoteDetail";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className='App'>
         <BrowserRouter>
           <Route exact path={'/'} component={Home} />
+          <Route exact path={'/notes/:id'} component={NoteDetail} />
         </BrowserRouter>
       </div>
     );
